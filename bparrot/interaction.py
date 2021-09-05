d93017c6ef34b3438367a82865e78ab04eca2b8d
Patrick Brennan <brennanpatrick20@gmail.com>
2021-09-05 13:15:32 -0400

    refactor location of 'ack' method

--- bparrot/interaction.py
+++ bparrot/interaction.py
@@ -77,15 +77,6 @@ class Interaction:
 
     def get_args(self) -> Tuple:
         return tuple((o.value for o in self.data.options))
-    
-    def ack(self):
-        """
-        Acknowledge the interaction, and show a "thinking" message. The
-        interaction must be edited later to display content.
-        """
-        return self.create_response(type_=1)
-    
-    think = ack
 
     def create_response(
         self,
@@ -140,6 +131,15 @@ class Interaction:
 
         self._responded = True
         return resp
+    
+    def ack(self):
+        """
+        Acknowledge the interaction, and show a "thinking" message. The
+        interaction must be edited later to display content.
+        """
+        return self.create_response(type_=1)
+    
+    think = ack
 
     async def followup(
         self,
