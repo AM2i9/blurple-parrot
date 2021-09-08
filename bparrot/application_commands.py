3066f9727ddf947e0d42cc12a02a46accfc1f84f
Patrick Brennan <brennanpatrick20@gmail.com>
2021-09-07 20:46:13 -0400

    Created better ApplicationCommand objects

---
+++ bparrot/application_commands.py
@@ -0,0 +1,191 @@
+import re
+
+from typing import List
+
+
+class CommandNameError(Exception):
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return (
+            f"Name {self.name} should only contain the following symbols: a-z 0-9 - _"
+        )
+
+
+class SlashOptionType:
+    SUB_COMMAND = 1
+    SUB_COMMAND_GROUP = 2
+    STRING = 3
+    INTEGER = 4
+    BOOLEAN = 5
+    USER = 6
+    CHANNEL = 7
+    ROLE = 8
+    MENTIONABLE = 9
+    NUMBER = 10
+
+
+class ApplicationCommandType:
+    CHAT_INPUT = 1
+    USER = 2
+    MESSAGE = 3
+
+
+class ApplicationCommand:
+
+    name: str
+
+    def __init__(self, type: ApplicationCommandType, **kwargs):
+        self.type = type
+        self.id = kwargs.get("id", 0)
+        self.application_id = kwargs.get("application_id", None)
+
+    def __eq__(self, other):
+        return (self.id == other.id) or (
+            self.type == other.type
+            and self.name == other.name
+            and self.description == other.description
+            and self.options == other.options
+        )
+
+    @classmethod
+    def from_dict(cls, data):
+        return cls(**data)
+
+
+class SlashChoice:
+    def __init__(self, name: str, value):
+        self.name = name
+        self.value = value
+
+    @classmethod
+    def from_dict(cls, data):
+        return cls(**data)
+
+    def to_dict(self):
+        return {"name": self.name, "value": self.value}
+
+
+class SlashOption:
+    def __init__(
+        self,
+        name: str,
+        description: str,
+        type: SlashOptionType = SlashOptionType.STRING,
+        required: bool = False,
+        choices: List[SlashChoice] = [],
+        options: List["SlashOption"] = [],
+    ):
+
+        if re.match("^[\w-]{1,32}$", name) is None and name.islower():
+            raise CommandNameError(name)
+
+        self.name = name
+        self.description = description
+        self.type = type
+        self.required = bool(required)
+        self.choices = choices
+        self.options = options
+
+    @classmethod
+    def from_dict(cls, data):
+        if data.get("choices"):
+            data["choices"] = [
+                SlashChoice.from_dict(choice) for choice in data["choices"]
+            ]
+        if data.get("options"):
+            data["options"] = [
+                SlashOption.from_dict(option) for option in data["options"]
+            ]
+        return cls(**data)
+
+    def to_dict(self):
+        data = {
+            "type": self.type,
+            "name": self.name,
+            "description": self.description,
+        }
+
+        if self.required:
+            data["required"] = self.required
+
+        if self.choices:
+            data["choices"] = [c.to_dict() for c in self.choices]
+
+        if self.options:
+            data["options"] = [o.to_dict() for o in self.options]
+
+        return data
+
+
+class SlashCommand(ApplicationCommand):
+    def __init__(
+        self,
+        name: str,
+        description: str,
+        options: List[SlashOption] = [],
+        default_permission: bool = True,
+        **kwargs,
+    ):
+        super().__init__(ApplicationCommandType.CHAT_INPUT, **kwargs)
+
+        if re.match("^[\w-]{1,32}$", name) is None and name.islower():
+            raise CommandNameError(name)
+
+        self.name = name
+        self.description = description
+        self.options = options
+        self.default_permission = default_permission
+
+    @classmethod
+    def from_dict(cls, data):
+        if data.get("options"):
+            data["options"] = [
+                SlashOption.from_dict(option) for option in data["options"]
+            ]
+        return cls(**data)
+
+    def to_dict(self):
+        data = {
+            "type": self.type,
+            "name": self.name,
+            "description": self.description,
+        }
+
+        if self.options:
+            data["options"] = [o.to_dict() for o in self.options]
+        if not self.default_permission:
+            data["default_permission"] = False
+
+        return data
+
+
+class UserCommand(ApplicationCommand):
+    def __init__(self, name: str, **kwargs):
+        super().__init__(ApplicationCommandType.USER, **kwargs)
+        self.name = name
+
+    def to_dict(self):
+        return {"type": self.type, "name": self.name}
+
+
+class MessageCommand(ApplicationCommand):
+    def __init__(self, name: str, **kwargs):
+        super().__init__(ApplicationCommandType.MESSAGE, **kwargs)
+        self.name = name
+
+    def to_dict(self):
+        return {"type": self.type, "name": self.name}
+
+
+def get_application_command(data: dict) -> ApplicationCommand:
+    type_ = data.pop("type")
+    if type_ == ApplicationCommandType.CHAT_INPUT:
+        return SlashCommand.from_dict(data)
+    elif type_ == ApplicationCommandType.USER:
+        return UserCommand.from_dict(data)
+    elif type_ == ApplicationCommandType.MESSAGE:
+        return MessageCommand.from_dict(data)
+    else:
+        return None
