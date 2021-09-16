2c434b49bb1788f571ef63b0b694664ba82bfc1e
Patrick Brennan <brennanpatrick20@gmail.com>
2021-09-16 16:42:20 -0400

    Polishing auth.py

--- bparrot/__init__.py
+++ bparrot/__init__.py
@@ -1,4 +1,5 @@
 from bparrot.client import Client
+from bparrot.auth import get_application_token
 from bparrot.core import *
 from bparrot.models import *
 from bparrot.interaction import *
--- bparrot/auth.py
+++ bparrot/auth.py
@@ -13,7 +13,7 @@ def get_application_token(client_id: int, client_secret: str, scopes: list = [])
     accessing your application commands. Aditional scopes can be added using
     the `scopes` parameter
 
-    OAuth2 credentials are possible more dangerous to have exposed than
+    OAuth2 credentials are possible more dangerous to have exposed  than
     a normal bot token, so take Discord's warning as said in their docs:
     https://discord.com/developers/docs/topics/oauth2#client-credentials-grant
     """
@@ -21,10 +21,12 @@ def get_application_token(client_id: int, client_secret: str, scopes: list = [])
     scopes = set(scopes)
     scopes.add("applications.commands")
 
-    data = aiohttp.FormData({
-        "grant_type": "client_credentials",
-        "scope": " ".join(scopes),
-    })
+    data = aiohttp.FormData(
+        {
+            "grant_type": "client_credentials",
+            "scope": " ".join(scopes),
+        }
+    )
 
     headers = {"Content-Type": "application/x-www-form-urlencoded"}
 
